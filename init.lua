ac2de15999b97840d1c4cf413abbfdadf0d48513
Ohm Desai <odesai840@gmail.com>
2026-07-22 19:00:05 -0400

    new plugins

--- init.lua
+++ init.lua
@@ -1,4 +1,5 @@
 vim.g.mapleader = ","
+vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
 
 require("config.options")
 require("config.lazy")
--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -6,5 +6,11 @@ return {
     config = function()
         require("nvim-tree").setup({})
         vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle file tree" })
+
+        vim.api.nvim_create_autocmd("VimEnter", {
+            callback = function()
+                require("nvim-tree.api").tree.open()
+            end,
+        })
     end,
 }
